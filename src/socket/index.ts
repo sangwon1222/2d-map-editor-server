0fac7327d400a30fbf424506d2d10acc6c21991d
sangwonlee <hkbvc1222@gmail.com>
2024-02-20 09:45:09 +0900

    UPDATE socket connect

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -7,7 +7,7 @@ const origin = isProduction ? 'http://lsw.kr' : '*'
 export class Socket{
   private mSocket: Server
   private mRobyUsers:any
-  private mRoomIndex: number
+  private mMoveTimer: any
   private mUserIdAry: string[]
 
   get userIdAry(): string[]{
@@ -18,7 +18,7 @@ export class Socket{
     // const { clientsCount } = (io.engine as any)
     this.mSocket = new Server(http,{cors:{ origin,credentials:isProduction}})
     this.mRobyUsers = {}
-    this.mRoomIndex = 0
+    this.mMoveTimer = {}
     this.mUserIdAry=[]
   }
   
@@ -33,52 +33,64 @@ export class Socket{
           return { socketId: id, nickname: data.nickname } 
         })
       }
+
+      const userNicknames: any = async () => {
+        const sockets = await this.mSocket.fetchSockets();
+        const data: { [key: string]: number[]} =  {}
+        for(let i=0; i<sockets.length; i++){
+          if(sockets[i].data.nickname) data[sockets[i].data.nickname] = sockets[i].data.path
+        }
+        return data
+      }
       
 
       this.mRobyUsers[socket.id] = true
       socket.data.roomName = '';
       socket.data.nickname = '';
+      socket.data.path = [];
       socket.data.rooms = [...(socket as any).adapter.sids]
 
-
-
-      socket.on('income', async ({ nickname })=>{
-        const data = await userInfo()
-        let ok = false
-        for(let i=0; i < data.length; i++){
-          if(data[i].nickname === nickname) {
-            ok = false 
-            break;
-          }
-          ok = true
-        }
-        
-        socket.data.nickname = ok ? nickname : ''
-        const socketUserList = ok ? await userInfo() : data
-        const myNickname = ok ? nickname : ''
-        const socketId = ok ? socket.id : ''
-
-        socket.emit('regist-user-id',{ ok, socketUserList, nickname: myNickname, socketId })
-        if(ok)socket.broadcast.emit('update-user-list',{ socketUserList, nickname: myNickname })
+      socket.on('income', async ({nickname, path})=>{
+        socket.data.nickname = nickname
+        socket.data.path = path
+        socket.data.timeout =[]
+        const users = await userNicknames()
+        socket.emit('insert-user',{ users })
+        socket.broadcast.emit('insert-user',{ users })
       })
 
-      socket.on("get-user-list",async ()=>{
-        const socketUserList = await userInfo()
-        socket.emit('get-user-list',{ socketUserList })
-      })
-
+      socket.on("insert-user", async () => {
+        const users = await userNicknames()
+        socket.emit('insert-user',{ users })
+        socket.broadcast.emit("insert-user", { users });
+      });
       
-      socket.on("add-chat", async ({nickname,chat,time}:{nickname:string,chat:string, time: string}) => {
-        socket.emit('add-chat',{ nickname,chat,time })
-        socket.broadcast.emit("add-chat", {nickname,chat,time});
+      socket.on("update-user-pos", async ({ nickname, path }) => {
+        socket.data.path = path
+        const users = await userNicknames()
+        socket.emit('update-user-pos',{ users })
+        socket.broadcast.emit("update-user-pos", { users });
       });
 
-      socket.on("update-map-json", async ({mapJson}) => {
-        socket.broadcast.emit("update-map-json", {
-          mapJson: mapJson,
-        });
+      socket.on("remove-prev-move", async () => {
+        for(let i=0; i < socket.data.timeout.length; i++){
+          clearTimeout(socket.data.timeout[i])
+        }
+        socket.data.timeout =[]
+    });
+
+      socket.on("user-move", async ({ nickname, pathList }:{nickname:string, pathList:number[][]}) => {
+          socket.data.path = pathList
+          socket.data.timeout = []
+          for(let i=0; i<pathList.length; i++){
+            socket.data.timeout[i] = setTimeout(() => {
+              const prev = i > 0 ? pathList[ i - 1 ] : pathList[ i ]
+              socket.emit('user-move',{ nickname, path: pathList[i]  ,prev })
+              socket.broadcast.emit("user-move", { nickname, path: pathList[i] , prev } );
+            }, 150*i);
+          }
       });
-    
+
       socket.on("leave-user", async () => {
         const users = await userInfo()
         socket.broadcast.emit("leave-user", {
